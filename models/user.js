b7bb78e4a98f157f2aea5b7d4be6154c8b24fd8d
sufyaan ahmad <sufyaanahmadx9x@gmail.com>
2025-04-13 16:39:15 +0530

    the first model have creted

---
+++ models/user.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const userschema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    accountType:{
+        type: String,
+        enum: ["Admin","student","instructor" ],
+        require: true
+    },
+    additionalDetail:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:"profile",
+    },
+    courses:[{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"Course"
+    }],
+    Image:{
+        type:String,
+        required:true,
+    },
+
+    courseProgress:[
+        {
+          type:mongoose.Schema.Types.ObjectId,
+          ref:"CourseProject",
+        }
+    ]
+});
+ module.exports=mongoose.model("user", userschema)
